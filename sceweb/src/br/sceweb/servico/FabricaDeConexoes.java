82939bbbd0b53c45096ac7cb812600a623192c79
DESKTOP-91EI809\esadv6 <engsoftware1@gmail.com>
2017-04-20 08:51:15 -0300

    UC01CadastrarEmpresa - refatoraçao da classe FabricaDeConexoes
    inclusao da classe ConfiguraDB

--- sceweb/src/br/sceweb/servico/FabricaDeConexoes.java
+++ sceweb/src/br/sceweb/servico/FabricaDeConexoes.java
@@ -1,22 +1,51 @@
 package br.sceweb.servico;
 
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import org.apache.log4j.Logger;
 
 import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.exceptions.jdbc4.CommunicationsException;
 
 public class FabricaDeConexoes {
 	Logger logger = Logger.getLogger(FabricaDeConexoes.class);
+	String url = "jdbc:mysql://localhost/sceweb";
+	String driver = "com.mysql.jdbc.Driver";
+	String usuario = "root";
+	String senha = "";
+	
+	public FabricaDeConexoes (ConfiguraDB configura){
+		this.url = configura.getUrl();
+		this.driver = configura.getDriver();
+		this.usuario = configura.getUsuario();
+		this.senha = configura.getSenha();
+	}
+	public FabricaDeConexoes(){
+		
+	}
 	public Connection getConnection(){
-		String url = "jdbc:mysql://localhost/sceweb";
+		
+		
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			return (Connection) DriverManager.getConnection(url,"root","");
+			Class.forName(driver);
+			return (Connection) DriverManager.getConnection(url,usuario,senha);
 			}
+		catch (CommunicationsException e){
+			logger.info("Exceção de comunicacao com o DB causa: " + e.getMessage());
+			
+			throw new RuntimeException(e); 
+		}
+		catch (SQLException e){
+			logger.info("Exceção geral causa SQLException: " + e.getMessage());
+			
+			throw new RuntimeException(e); 
+		}
 		catch (Exception e){
-			logger.info("SQLException na classe FabricaDeConexoes causa: " + e.getMessage());
+			logger.info("Exceção geral causa: " + e.getMessage());
+			
 			throw new RuntimeException(e); 
 		}
 	}
 }
+
--- sceweb/teste/br/sceweb/teste/UC01CadastrarEmpresa.java
+++ sceweb/teste/br/sceweb/teste/UC01CadastrarEmpresa.java
@@ -9,10 +9,15 @@ import org.junit.Test;
 
 import br.sceweb.modelo.Empresa;
 import br.sceweb.modelo.EmpresaDAO;
+import br.sceweb.servico.ConfiguraDB;
+import br.sceweb.servico.FabricaDeConexoes;
 
 public class UC01CadastrarEmpresa {
 	static EmpresaDAO empresaDAO;
 	static Empresa empresa;
+	static ConfiguraDB configuraDB;
+	static FabricaDeConexoes fabricaDeConexoes;
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		empresaDAO = new EmpresaDAO();
@@ -23,49 +28,75 @@ public class UC01CadastrarEmpresa {
 		empresa.setEndereco("rua taquari");
 		empresa.setTelefone("2222");
 	}
+
 	@Before
 	public void setUp() throws Exception {
 		empresaDAO.exclui("89424232000180");
 	}
+
 	@Test
 	public void CT01UC01FBCadastra_com_sucesso() {
-		assertEquals(1,empresaDAO.adiciona(empresa));
-		
+		assertEquals(1, empresaDAO.adiciona(empresa));
+
 	}
-	
-	@Test (expected = RuntimeException.class)
-	public void CT02UC01A2Cadastra_cnpj_ja_cadastrado(){
+
+	@Test(expected = RuntimeException.class)
+	public void CT02UC01A2Cadastra_cnpj_ja_cadastrado() {
 		empresaDAO.adiciona(empresa);
 		empresaDAO.adiciona(empresa);
 	}
 
 	@Test
-	public void CT03UC01A3Cadastra_empresa_cnpj_invalido(){
+	public void CT03UC01A3Cadastra_empresa_cnpj_invalido() {
 		Empresa empresa2 = new Empresa();
-		try{
+		try {
 			empresa2.setCnpj("8942423200018");
 			fail("deveria disparar uma expection");
-		} catch (Exception e){
+		} catch (Exception e) {
 			assertEquals("CNPJ inválido!", e.getMessage());
 		}
 	}
-	
+
 	@Test
-	public void CT04UC01A4Cadastra_empresa_sem_nome(){
+	public void CT04UC01A4Cadastra_empresa_sem_nome() {
 		Empresa empresa3 = new Empresa();
-		try{
+		try {
 			empresa3.setNomeDaEmpresa("");
 			fail("deveria disparar uma expection");
-		} catch (Exception e){
+		} catch (Exception e) {
 			assertEquals("nome da empresa inválido!", e.getMessage());
 		}
 	}
-	
+
+	@Test
+	public void CT05_conecta_db_com_sucesso() {
+		String url = "jdbc:mysql://localhost/sceweb";
+		String driver = "com.mysql.jdbc.Driver";
+		String usuario = "root";
+		String senha = "";
+		configuraDB = new ConfiguraDB(url, driver, usuario, senha);
+		fabricaDeConexoes = new FabricaDeConexoes(configuraDB);
+		assertNotNull(fabricaDeConexoes.getConnection());
+	}
+
+	@Test
+	public void CT06_conecta_db_com_erro_url() {
+		String url = "jdbc:mysql://localhost/sceweb1";
+		String driver = "com.mysql.jdbc.Driver";
+		String usuario = "root";
+		String senha = "";
+		configuraDB = new ConfiguraDB(url, driver, usuario, senha);
+		fabricaDeConexoes = new FabricaDeConexoes(configuraDB);
+		try {
+			fabricaDeConexoes.getConnection();
+		} catch (Exception e) {
+			assertEquals("com.mysql.jdbc.exceptions.jdbc4.MySQLSyntaxErrorException: Unknown database 'sceweb1'", e.getMessage());
+		}
+	}
+
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 		empresaDAO.exclui("89424232000180");
 	}
-
-
 
 }
