baa4dfbd9eb94e232651cd52254bdba513dabbe9
DESKTOP-91EI809\esadv6 <engsoftware1@gmail.com>
2017-04-23 09:02:22 -0300

    Manutencao corretiva - inclusão do delete no script de teste
    UC05CadastrarConvenio - inclusao do delete empresa

--- sceweb/teste/br/sceweb/teste/UC05CadastrarConvenioGUI.java
+++ sceweb/teste/br/sceweb/teste/UC05CadastrarConvenioGUI.java
@@ -18,6 +18,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import br.sceweb.modelo.ConvenioDAO;
+import br.sceweb.modelo.Empresa;
 import br.sceweb.modelo.EmpresaDAO;
 
 public class UC05CadastrarConvenioGUI {
@@ -26,21 +27,32 @@ public class UC05CadastrarConvenioGUI {
   private boolean acceptNextAlert = true;
   private StringBuffer verificationErrors = new StringBuffer();
   private static FormConvenio formConvenio;
+  private static EmpresaDAO empresaDAO;
+  private static Empresa empresa;
   @BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		System.setProperty("webdriver.gecko.driver",
+	    System.setProperty("webdriver.gecko.driver",
 				"C:/Users/esadv6/git/20171S_fatec3/sceweb/WebContent/WEB-INF/lib/geckodriver.exe");
 				
 		driver = new FirefoxDriver();
 		baseUrl = "http://localhost:8080/";
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+		empresaDAO = new EmpresaDAO();
+		empresa = new Empresa();
+		empresa.setNomeDaEmpresa("Open Informatica Ltda.");
+		empresa.setCnpj("89424232000180");
+		empresa.setNomeFantasia("Open Informatica");
+		empresa.setEndereco("Rua Taquari");
+		empresa.setTelefone("2222");
+		empresaDAO.adiciona(empresa);
 		formConvenio = new FormConvenio(driver); 
 	}
 
   @Test
   public void testUC05CadastrarConvenio() throws Exception {
 	    formConvenio.abre();
-		formConvenio.cadastra("53236636000101","21/04/2017","21/04/2018");
+		formConvenio.cadastra("89424232000180","21/04/2017","21/04/2018");
 		try {
 			WebDriverWait wait = new WebDriverWait(driver, 10);
 			wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("mensagem")));
@@ -53,7 +65,8 @@ public class UC05CadastrarConvenioGUI {
   @After
   public void tearDown() throws Exception {
 	ConvenioDAO convenioDAO = new ConvenioDAO();
-	convenioDAO.exclui("50658639000137");
+	convenioDAO.exclui("89424232000180");
+	empresaDAO.exclui("89424232000180");
     driver.quit();
     String verificationErrorString = verificationErrors.toString();
     if (!"".equals(verificationErrorString)) {
